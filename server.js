1ff8c96a4b4cef7b61aa1ae802ed0da72eaf441e
Kevin Hubbard <dgkallday98@gmail.com>
2019-09-19 16:49:56 -0400

    updated db object with date & hole #

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://heroku_26mvj5w7:818ip97apf8gjh0eq5j7gvj5ht@ds217548.mlab.com:17548/heroku_26mvj5w7', {useNewUrlParser: true});
 const app = express();
-
+const Game = mongoose.model('Game', {date: Date, courseName: String, totalHoles: Number, hole: Array, totalStrokes: Number, score: Number});
 const port = process.env.PORT || 8888;
 
 
@@ -19,8 +19,7 @@ app.get('/', (req,res) => {
 
 app.post('/', (req,res) => {
 	console.log(req.body);
-	const Game = mongoose.model('Game', {courseName: String, totalHoles: Number, hole: Array, totalStrokes: Number, score: Number});
-	const newGame = new Game({courseName: req.body.courseName, totalHoles: req.body.totalHoles, totalStrokes: req.body.totalStrokes, score: req.body.score});
+	const newGame = new Game({date: Date.now(), courseName: req.body.courseName, totalHoles: req.body.totalHoles, hole: req.body.hole, totalStrokes: req.body.totalStrokes, score: req.body.score});
 	newGame.save();
 
 });
