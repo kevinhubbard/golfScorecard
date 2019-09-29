9ca879dbab6188225042b3b0f932f159ee2e6e16
Kevin Hubbard <dgkallday98@gmail.com>
2019-09-29 17:01:31 -0400

    show indepth scorecard

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ const app = express();
 const port = process.env.PORT || 8888;
 
 app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/public/views');
+app.set('views', __dirname + '/mvc/views');
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 //load routes modules
-const index = require('./controller/index.js');
-const scores = require('./controller/scores.js');
-const api = require('./controller/api.js');
+const index = require('./mvc/controller/index.js');
+const scores = require('./mvc/controller/scores.js');
+const api = require('./mvc/controller/api.js');
 
 app.use('/', index);
 app.use('/scores', scores);
